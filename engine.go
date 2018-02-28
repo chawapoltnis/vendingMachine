f8d601972bf286ac27c857313fc05694cc0e74dd
chawapoltnis <chawapol.it@tnis.com>
2018-02-28 10:19:49 +0700

    create package VendingMachine

---
+++ engine.go
@@ -0,0 +1,53 @@
+package vendingMachine
+
+type VendingMachine struct {
+	insertedMoney int
+	coins map[string]int
+	items map[string]int
+}
+
+func NewVendingMachine(coins, items map[string]int) *VendingMachine{
+	return &VendingMachine{coins:coins, items:items}
+}
+
+func (m VendingMachine) InsertedMoney() int {
+	return m.insertedMoney
+}
+
+func (m *VendingMachine) InserCoin(coin string) {
+	m.insertedMoney += m.coins[coin]
+}
+
+func (m *VendingMachine) SelectSD() string {
+	price:=m.items["SD"]
+	m.insertedMoney-=price
+	return "SD" + m.change(m.insertedMoney)
+}
+
+func (m *VendingMachine) SelectCC() string {
+	price:=m.items["CC"]
+	m.insertedMoney-=price
+	return "CC" + m.change(m.insertedMoney)
+}
+
+func (m *VendingMachine) change(c int) string{
+	var str string
+	values := [...]int{10,5,2,1}
+	coins := [...]string{"T","F","TW","O"}
+	
+	for i:=0;i<len(values);i++ {
+		if c>=values[i] {
+			str+=", "+coins[i]
+			c-=values[i]
+			m.insertedMoney-=values[i]
+			i--
+		}		
+	}
+	return str
+}
+
+func (m *VendingMachine) CoinReturn() string {
+	coins := m.change(m.insertedMoney)
+	m.insertedMoney=0
+	return coins[2:len(coins)]
+}
